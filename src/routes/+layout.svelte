df436f6ab7fac0c54c66076861e6c55f9daaafb3
Imad <imadchowdhury01@gmail.com>
2025-06-14 16:57:45 +0100

    Add routing and navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { page } from '$app/state';
+	import '../app.css';
+
+	const routes = { '/': 'Home', '/weather': 'Weather' };
+</script>
+
+<div class="layout-container">
+	<nav class="navbar">
+		{#each Object.entries(routes) as [route, routeName]}
+			<a class="navbar-link" class:navbar-link-selected={page.url.pathname === route} href={route}
+				>{routeName}</a
+			>
+		{/each}
+	</nav>
+
+	<main class="layout-main">
+		<slot />
+	</main>
+
+	<footer class="footer">
+		<p>Made by Imad</p>
+	</footer>
+</div>
+
+<style>
+	.layout-container {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.layout-main {
+		flex: 1;
+	}
+
+	.navbar {
+		border-bottom: 1px solid var(--gray);
+		display: flex;
+		gap: 0.1rem;
+		padding: 0.1rem 0.1rem 0 0.1rem;
+	}
+
+	.navbar-link {
+		text-decoration: none;
+		color: var(--black);
+		padding: 0 0.5rem;
+		border-radius: 3px 3px 0px 0px;
+	}
+
+	.navbar-link-selected {
+		background: var(--gray-highlight);
+	}
+
+	.navbar-link:hover {
+		background: var(--gray-highlight);
+	}
+
+	.footer {
+		border-top: 1px solid var(--gray);
+		text-align: center;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container">
+	<h1 class="title">Demos</h1>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, maiores, sunt quasi
+		voluptatibus rem dolore praesentium esse neque cum perferendis necessitatibus magnam, asperiores
+		eum odio architecto atque officiis blanditiis aut!
+	</p>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		max-width: 900px;
+		margin: 0 auto;
+	}
+
+	.title {
+		text-align: center;
+	}
+</style>
