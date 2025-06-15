ca6eb075e41ad83ff8a3f4fb571b744a57c104e0
Imad-C <imadchowdhury01@gmail.com>
2025-06-15 22:53:07 +0100

    Style the Weather page

--- src/routes/weather/+page.svelte
+++ src/routes/weather/+page.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
-	let location: string | null = null;
-	let locationWeather: WeatherData | null = null;
-
 	interface WeatherData {
 		location: string;
 		fullLocation: string;
-		temp: string;
-		max: string;
-		min: string;
-		tomTemp: string;
+		temp: number;
+		max: number;
+		min: number;
+		tomTemp: number;
 	}
 
+	let location: string | null = null;
+	let loadingLoactionWeather = false;
+	let locationWeather: WeatherData | null = null;
+
 	async function fetchWeather(location: string) {
+		loadingLoactionWeather = true;
+
 		const res = await fetch('/api/weather', {
 			method: 'POST',
 			headers: {
@@ -22,6 +25,7 @@
 		const data = await res.json();
 
 		locationWeather = data;
+		loadingLoactionWeather = false;
 	}
 </script>
 
@@ -34,10 +38,18 @@
 		</div>
 		<div class="right-panel">
 			{#if locationWeather}
-				<h2>{locationWeather?.location}</h2>
-				<p>Min: {locationWeather?.min}</p>
-				<p>Max: {locationWeather?.max}</p>
-				<p>Tomorrow: {locationWeather?.tomTemp}</p>
+				<h2 class="location-heading">{locationWeather?.location}</h2>
+				<div class="today-panel">
+					<p class="temperature">{locationWeather?.temp}°C</p>
+					<div>
+						<p>Min: {locationWeather?.min}°C</p>
+						<p>Max: {locationWeather?.max}°C</p>
+					</div>
+				</div>
+
+				<p class="tomorrow">Tomorrow: {locationWeather?.tomTemp}°C</p>
+			{:else if loadingLoactionWeather}
+				<p>Loading...</p>
 			{:else}
 				<p>Search for Somewhere!</p>
 			{/if}
@@ -55,15 +67,35 @@
 
 	.title {
 		text-align: center;
+		margin-bottom: 3rem;
 	}
 
 	.panel-container {
 		display: flex;
 		justify-content: space-around;
+		text-align: center;
+		align-items: center;
 	}
 
 	.left-panel,
 	.right-panel {
 		flex: 1;
+	}
+
+	.right-panel {
+		padding: 1rem;
+		background: var(--gray-highlight);
+		border-radius: 8px;
+	}
+
+	.today-panel {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	.temperature {
+		font-size: 4rem;
 	}
 </style>
