d9a9549a371f120ae43422e48ec8f781ff6eb62e
Imad-C <imadchowdhury01@gmail.com>
2025-08-29 18:52:05 +0100

    Make cards draggable

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,10 +1,58 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
+	import { onMount, type Snippet } from 'svelte';
 
-	let { content, title }: { content: Snippet; title?: string } = $props();
+	let {
+		content,
+		title,
+		top = '0px',
+		left = '0px'
+	}: { content: Snippet; title?: string; top?: string; left?: string } = $props();
+
+	let container: HTMLDivElement;
+	let dragger: HTMLImageElement;
+	let pos1 = 0;
+	let pos2 = 0;
+	let pos3 = 0;
+	let pos4 = 0;
+
+	onMount(() => {
+		dragger.onmousedown = dragMouseDown;
+	});
+
+	function dragMouseDown(e: MouseEvent) {
+		e.preventDefault();
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		document.onmousemove = elementDrag;
+		console.log('dragMouseDown');
+	}
+
+	function elementDrag(e: MouseEvent) {
+		e.preventDefault();
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		container.style.top = container.offsetTop - pos2 + 'px';
+		container.style.left = container.offsetLeft - pos1 + 'px';
+		console.log('elementDrag');
+	}
+
+	function closeDragElement() {
+		document.onmouseup = null;
+		document.onmousemove = null;
+		console.log('closeDragElement');
+	}
 </script>
 
-<div class="container">
+<div class="container" bind:this={container} style={`top: ${top}; left: ${left};`}>
+	<img
+		src="/drag.svg"
+		alt="An icon to indicate a draggable UI element."
+		class="dragger"
+		bind:this={dragger}
+	/>
 	<p class="title">{title}</p>
 	<div class="content">
 		{@render content()}
@@ -13,13 +61,29 @@
 
 <style>
 	.container {
+		position: absolute;
 		border: solid var(--colour-silver) 3px;
 		border-radius: var(--border-radius-medium);
 		box-shadow: 0 0 2px rgba(0, 0, 0, 0.4);
+		min-width: 300px;
 	}
 
 	.container > * {
 		padding: var(--spacing-small);
+	}
+
+	.dragger {
+		width: 20px;
+		height: 20px;
+		position: absolute;
+		top: -15px;
+		left: -15px;
+		transition: transform 0.7s ease-in-out;
+	}
+
+	.dragger:hover {
+		cursor: grab;
+		transform: rotate(180deg);
 	}
 
 	.title {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import Card from '$components/Card.svelte';
+	import Search from '$components/Search.svelte';
 </script>
 
 <div class="container">
-	<Card title="Weather">
+	<Card title="Weather" top="300px" left="300px">
 		{#snippet content()}
 			<p>
 				A <a href="/weather">weather app</a> using the
 				<a href="https://www.visualcrossing.com/">Visual Crossing</a> API.
 			</p>
+			<Search value="null" handler={() => {}} />
 		{/snippet}
 	</Card>
 
-	<Card title="Snake">
+	<Card title="Snake" top="100px" left="700px">
 		{#snippet content()}
 			<p>
 				A <a href="/snake">game of snake</a> using a HTML canvas element.
@@ -23,9 +25,10 @@
 
 <style>
 	.container {
-		display: flex;
+		/* display: flex;
 		flex-direction: column;
 		align-items: flex-start;
-		gap: var(--spacing-medium);
+		gap: var(--spacing-medium); */
+		position: relative;
 	}
 </style>
