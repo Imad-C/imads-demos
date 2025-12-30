c5aebb469233d9080cc4881ca8f0e45de3587dfc
Imad-C <imadchowdhury01@gmail.com>
2025-12-30 18:57:35 +0000

    Make snake score look prettier

--- src/routes/snake/+page.svelte
+++ src/routes/snake/+page.svelte
@@ -21,7 +21,15 @@
 
 	<div class="menu">
 		<div class="high-current">
-			<Circle text={gameScore.toString()} />
+			<div class="score">
+				<p>Current Score</p>
+				<p class="score-number">{gameScore}</p>
+			</div>
+
+			<div class="score">
+				<p>High Score</p>
+				<p class="score-number">{highScore}</p>
+			</div>
 		</div>
 	</div>
 </div>
@@ -34,18 +42,32 @@
 	}
 
 	.menu {
-		/* background: var(--colour-platinum); */
 		border-radius: var(--border-radius-medium);
 		padding: var(--spacing-small);
-		flex: 1;
 	}
 
 	.high-current {
 		display: flex;
-		justify-content: space-between;
+		gap: var(--spacing-large);
 	}
 
 	.score {
-		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--spacing-small);
+	}
+
+	.score-number {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: var(--spacing-medium);
+		border: solid lightgreen 5px;
+		border-radius: 50%;
+		width: 4.5rem;
+		height: 4.5rem;
+		font-size: 3rem;
+		box-shadow: 0px 0px 0px 2px darkgreen;
 	}
 </style>
